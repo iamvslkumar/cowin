7e226deb7f0a40ca8289eb32f2a30c286dc8d377
iamvslkumar <39222923+iamvslkumar@users.noreply.github.com>
2021-05-10 16:10:42 +0530

    daterange

--- src/main/java/com/cowin/gov/service/CowinService.java
+++ src/main/java/com/cowin/gov/service/CowinService.java
@@ -1,12 +1,24 @@
 package com.cowin.gov.service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.TimeZone;
 
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,22 +40,33 @@ public class CowinService {
 		 * restTemplate.getForEntity(fooResourceUrl, DistrictResponse.class);
 		 */
 		try {
-            RestTemplate restTemplate = new RestTemplate();
+//            RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
             headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
             HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
+            CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            requestFactory.setHttpClient(httpClient);
+            RestTemplate restTemplate = new RestTemplate(requestFactory);
+            
             ResponseEntity<StateResponse>  stateResponse = restTemplate.exchange("https://cdn-api.co-vin.in/api/v2/admin/location/districts/9", HttpMethod.GET,entity,StateResponse.class);
             
+            
+            for(String date : getCalendarDate()) {
+            	System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4");
+            	System.out.println(date);
+            	System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4");
             for(District district : stateResponse.getBody().getDistricts()) {
             
-            ResponseEntity<DistrictResponse>  response = restTemplate.exchange("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+district.getDistrictId()+"&date=11s-05-2021", HttpMethod.GET,entity,DistrictResponse.class);
+            ResponseEntity<DistrictResponse>  response = restTemplate.exchange("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+district.getDistrictId()+"&date="+"10-05-2021", HttpMethod.GET,entity,DistrictResponse.class);
             DistrictResponse responseEntity = response.getBody();
             for(Session session: responseEntity.getSessions()) {
 //            	if(session.getMinAgeLimit()==18) {
 //            		//System.out.println(session);
 //            		System.out.println("##########################");
+//            		System.out.println("Center District :" + session.getDistrictName());
 //            		System.out.println("Center Name :" + session.getName());
 //            		System.out.println("Center Address :" + session.getAddress());
 //            		System.out.println("Center Pincode :" + session.getPincode());
@@ -60,6 +83,7 @@ public class CowinService {
             	if(session.getMinAgeLimit()==45 && session.getVaccine().equalsIgnoreCase("COVAXIN")) {
             		//System.out.println(session);
             		System.out.println("##########################");
+            		System.out.println("Center District :" + session.getDistrictName());
             		System.out.println("Center Name :" + session.getName());
             		System.out.println("Center Address :" + session.getAddress());
             		System.out.println("Center Pincode :" + session.getPincode());
@@ -78,6 +102,7 @@ public class CowinService {
             }
 //            System.out.println(responseEntity.getSessions());
             }
+            }
             
         } catch (Exception ex) {
            ex.printStackTrace();
@@ -86,6 +111,29 @@ public class CowinService {
 		
 		
 		return null;
+	}
+	
+	
+	private List<String> getCalendarDate(){
+		
+		List<String> dateRange = new  ArrayList<String>();
+		Calendar start = new GregorianCalendar(2021,4,10);;
+		Calendar end = new GregorianCalendar(2021,4,18);;
+		
+		while(start.before(end) || start.equals(end)) {
+			Date result = start.getTime();
+			dateRange.add(getDateValue(result));
+			start.add(Calendar.DATE, 1);
+		}
+		return dateRange;
+	}
+
+
+	private String getDateValue(Date date) {
+		SimpleDateFormat format= new SimpleDateFormat("dd-MM-yyyy");
+		format.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
+		String formattedDate = format.format(date);
+		return formattedDate;
 	}
 
 }
